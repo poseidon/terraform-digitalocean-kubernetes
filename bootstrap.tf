94fd7935a450ba01eb6e9012aada4d692aa9b9fe
Dalton Hubble <dghubble@gmail.com>
2021-05-13 10:56:00 -0700

    Update Kubernetes from v1.21.0 to v1.21.1
    * https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG/CHANGELOG-1.21.md#v1211

--- bootstrap.tf
+++ bootstrap.tf
@@ -1,6 +1,6 @@
 # Kubernetes assets (kubeconfig, manifests)
 module "bootstrap" {
-  source = "git::https://github.com/poseidon/terraform-render-bootstrap.git?ref=079b348bf7d9580f4459769921bfa0c5602623a0"
+  source = "git::https://github.com/poseidon/terraform-render-bootstrap.git?ref=4c660afd871a5f87f6dd84f01504a0ed1bdcc94f"
 
   cluster_name = var.cluster_name
   api_servers  = [format("%s.%s", var.cluster_name, var.dns_zone)]
